ae621e81cad651da0254a5843afe6f5ebae39b01
taylor.fish <contact@taylor.fish>
2022-05-07 11:08:43 -0700

    Formatting

--- src/cfmakeraw.rs
+++ src/cfmakeraw.rs
@@ -3,31 +3,29 @@
  * C Library (specifically, the file termios/cfmakeraw.c). That code is
  * covered by the following copyright and license notice:
  *
- *    Copyright (C) 1992-2019 Free Software Foundation, Inc.
- *    This file is part of the GNU C Library.
+ *     Copyright (C) 1992-2019 Free Software Foundation, Inc.
+ *     This file is part of the GNU C Library.
  *
- *    The GNU C Library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ *     The GNU C Library is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU Lesser General Public
+ *     License as published by the Free Software Foundation; either
+ *     version 2.1 of the License, or (at your option) any later version.
  *
- *    The GNU C Library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ *     The GNU C Library is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with the GNU C Library; if not, see
- *    <http://www.gnu.org/licenses/>.
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with the GNU C Library; if not, see
+ *     <http://www.gnu.org/licenses/>.
  *
  * All modifications in this file relative to the original GNU C Library
  * code are released under the same license as that code.
  */
 
-use nix::sys::termios::{
-    ControlFlags, InputFlags, LocalFlags, OutputFlags,
-    SpecialCharacterIndices::*, Termios,
-};
+use nix::sys::termios::{ControlFlags, InputFlags, LocalFlags, OutputFlags};
+use nix::sys::termios::{SpecialCharacterIndices, Termios};
 
 pub fn cfmakeraw(t: &mut Termios) {
     t.input_flags &= !(InputFlags::IGNBRK
@@ -47,6 +45,6 @@ pub fn cfmakeraw(t: &mut Termios) {
     t.control_flags &= !(ControlFlags::CSIZE | ControlFlags::PARENB);
     t.control_flags |= ControlFlags::CS8;
     // read returns when one char is available.
-    t.control_chars[VMIN as usize] = 1;
-    t.control_chars[VTIME as usize] = 0;
+    t.control_chars[SpecialCharacterIndices::VMIN as usize] = 1;
+    t.control_chars[SpecialCharacterIndices::VTIME as usize] = 0;
 }
